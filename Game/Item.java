9fc40d20c3568b18796448bddfc4cc7e1571dce3
KT5391205 <32402569+KT5391205@users.noreply.github.com>
2017-11-15 17:13:50 -0800

    0.0.02
    Added Item Broom

--- Game/Item.java
+++ Game/Item.java
@@ -1,4 +1,4 @@
-public class Item
+public class Item extends Items
 {
     public Item()
     {
--- Game/ItemList.java
+++ Game/ItemList.java
@@ -2,5 +2,14 @@ import java.util.HashMap;
 
 public class ItemList
 {
-    HashMap<Integer, Item> itemlist = new HashMap<Integer, Item>();
+    HashMap<Integer, Item> itemmap = new HashMap<Integer, Item>();
+    public ItemList()
+    {
+        Item Broom = new Item();
+        Broom.setName("Broom");
+        Broom.setDesc("a broom");
+        Broom.setValue(5);
+        Broom.setId(3001);
+        itemmap.put(0, Broom);
+    }
 }

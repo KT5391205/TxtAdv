e1f92dba8a355d974f22a44a9f5a8eaebc850872
Jojo <kt5391205@gmail.com>
2017-12-08 14:47:53 -0800

    Kathia Day
    asdf

--- Game/ClassList.java
+++ Game/ClassList.java
@@ -12,6 +12,40 @@ public class ClassList
         Barbarian.setId(4001);
         Barbarian.setSkills(3, 7, -1, -1);
         classMap.put(Barbarian.getId(), Barbarian);
+        
+        //Bard
+        CharacterClass Bard = new CharacterClass();
+        Bard.setName("bard");
+        Bard.setId(4002);
+        Bard.setSkills(4, 12, 13, -1);
+        classMap.put(Bard.getId(), Bard);
+        
+        //Cleric
+        CharacterClass Cleric = new CharacterClass();
+        Cleric.setName("cleric");
+        Cleric.setId(4003);
+        Cleric.setSkills(2, 14, -1, -1);
+        classMap.put(Cleric.getId(), Cleric);
+        
+        //Fighter
+        CharacterClass Fighter = new CharacterClass();
+        Fighter.setName("fighter");
+        Fighter.setId(4004);
+        Fighter.setSkills(3, 11, -1, -1);
+        classMap.put(Fighter.getId(), Fighter);
+        
+        //Monk
+        CharacterClass Monk = new CharacterClass();
+        Monk.setName("monk");
+        Monk.setId(4005);
+        Monk.setSkills(6, 11, -1, -1);
+        classMap.put(Monk.getId(), Monk);
+        
+        //Ranger
+        CharacterClass Ranger = new CharacterClass();
+        Ranger.setName("ranger");
+        Ranger.setId(4006);
+        Ranger.setSkills(6,6,6,6);
     }
     
     public CharacterClass getCharacterClass(int i)
